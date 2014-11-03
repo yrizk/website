bb6ec57a391141416aa57349e3340fcb2e71de79
yrizk <rizk.yohanna@gmail.com>
2014-11-02 23:27:07 -0500

    added the themeforest code and documentation

---
+++ HTML/js/style-change.js
@@ -0,0 +1,89 @@
+    //Orange
+    $("#click-orange").click(function() {
+
+        $('.e-campuz, .intro-text h1 span, .nest-address, .footer-above, .footer-below').css({
+            "background": "#ECA928"
+        });
+        $('.border-color').attr('style', 'border: 4px solid #ECA928!important');
+        $('.arrow-down').attr('style', 'border-top: 50px solid #ECA928!important');
+        $('.resume-content h3').attr('style', 'border-left: 28px solid #ECA928!important');
+        $('.navbar-brand, .title-change-color').attr('style', 'color: #ECA928!important');
+        $('.page-scroll a:hover').attr('style', 'color: #ECA928!important');
+        $('.btn-social').attr('style', 'color: #ECA928!important');
+        $('.text-title').attr('style', 'color: #ECA928!important');
+    });
+    //blue
+    $("#click-blue").click(function() {
+
+        $('.e-campuz, .intro-text h1 span, .nest-address, .footer-above, .footer-below').css({
+            "background": "#05C1FD"
+        });
+        $('.border-color').attr('style', 'border: 4px solid #05C1FD!important');
+        $('.arrow-down').attr('style', 'border-top: 50px solid #05C1FD!important');
+        $('.resume-content h3').attr('style', 'border-left: 28px solid #05C1FD!important');
+        $('.navbar-brand, .title-change-color').attr('style', 'color: #05C1FD!important');
+        $('.page-scroll a:hover').attr('style', 'color: #05C1FD!important');
+        $('.btn-social').attr('style', 'color: #05C1FD!important');
+        $('.text-title').attr('style', 'color: #05C1FD!important');
+    });
+
+    //Green
+    $("#click-green").click(function() {
+
+        $('.e-campuz, .intro-text h1 span, .nest-address, .footer-above, .footer-below').css({
+            "background": "#1abc9c"
+        });
+        $('.border-color').attr('style', 'border: 4px solid #1abc9c!important');
+        $('.arrow-down').attr('style', 'border-top: 50px solid #1abc9c!important');
+        $('.resume-content h3').attr('style', 'border-left: 28px solid #1abc9c!important');
+        $('.navbar-brand, .title-change-color').attr('style', 'color: #1abc9c!important');
+        $('.page-scroll a:hover').attr('style', 'color: #1abc9c!important');
+        $('.btn-social').attr('style', 'color: #1abc9c!important');
+        $('.text-title').attr('style', 'color: #1abc9c!important');
+    });
+    //Purple
+    $("#click-purple").click(function() {
+
+        $('.e-campuz, .intro-text h1 span, .nest-address, .footer-above, .footer-below').css({
+            "background": "#8e44ad"
+        });
+        $('.border-color').attr('style', 'border: 4px solid #8e44ad!important');
+        $('.arrow-down').attr('style', 'border-top: 50px solid #8e44ad!important');
+        $('.resume-content h3').attr('style', 'border-left: 28px solid #8e44ad!important');
+        $('.navbar-brand, .title-change-color').attr('style', 'color: #8e44ad!important');
+        $('.page-scroll a:hover').attr('style', 'color: #8e44ad!important');
+        $('.btn-social').attr('style', 'color: #8e44ad!important');
+        $('.text-title').attr('style', 'color: #8e44ad!important');
+    });
+    //Red
+    $("#click-red").click(function() {
+
+        $('.e-campuz, .intro-text h1 span, .nest-address, .footer-above, .footer-below').css({
+            "background": "#e74c3c"
+        });
+        $('.border-color').attr('style', 'border: 4px solid #e74c3c!important');
+        $('.arrow-down').attr('style', 'border-top: 50px solid #e74c3c!important');
+        $('.resume-content h3').attr('style', 'border-left: 28px solid #e74c3c!important');
+        $('.navbar-brand, .title-change-color').attr('style', 'color: #e74c3c!important');
+        $('.page-scroll a:hover').attr('style', 'color: #e74c3c!important');
+        $('.btn-social').attr('style', 'color: #e74c3c!important');
+        $('.text-title').attr('style', 'color: #e74c3c!important');
+    });
+    //Background
+    $("#click-bg-dark").click(function() {
+
+        $('body').attr('style', 'background: #555!important');
+    });
+    $("#click-bg-light").click(function() {
+
+        $('body').attr('style', 'background: #cdcdcd!important');
+    });
+
+
+//unused
+  $("#click-bg-blue").click(function() {
+        $('body').attr('style', 'background: url(img/blue-sky.jpg) no-repeat center center fixed!important; background-size: cover!important; -o-background-size: cover!important;-moz-background-size: cover!important;-webkit-background-size: cover!important;');
+    });
+
+
+    
