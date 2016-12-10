30e72350e2acca82259cc396cdcab353475c02f6
Yohan Rizk <yohan.rizk@gmail.com>
2016-12-09 23:32:34 -0500

    added templates. add url route for post_list. fixed the post model. added a couple sample posts

--- blog/models.py
+++ blog/models.py
@@ -14,8 +14,8 @@ class Post(models.Model):
 	        blank=True, null=True)
 
 	def publish(self):
-	    self.published_date = timezone.now()
-	    self.save()
+		self.published_date = timezone.now()
+		self.save()
 
 	def __str__(self):
 	    return self.title
